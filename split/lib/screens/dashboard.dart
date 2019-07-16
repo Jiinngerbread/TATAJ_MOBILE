03b47a1c43cbae46ef11c0c4f71c5d05ecd3d8d3
Jiinngerbread <jiinngerbread@gmail.com>
2019-07-16 15:22:15 -0500

    Update dashboard.dart

--- split/lib/screens/dashboard.dart
+++ split/lib/screens/dashboard.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
-class dashboard {
-  
+  class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Dashboard"), backgroundColor: Colors.deepOrange),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new IconButton(
+                icon: new Icon(Icons.home, color: Colors.blue),
+                iconSize: 70.0,
+                onPressed: () {Navigator.of(context).pushNamed("/Profile");}
+              ),
+              new Text("Dash")
+            ]
+          )
+        )
+      )
+    );
+  }
+}
 
   
   
-}
